c24efcc61af94e778960b4c7bae3b3120e679551
Vicen Dominguez <vicente.dominguez@flywire.com>
2025-04-07 20:25:12 +0200

    sha256sum fixed

--- simplehttpserver.rb
+++ simplehttpserver.rb
@@ -5,10 +5,10 @@ class Simplehttpserver < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/projectdiscovery/simplehttpserver/releases/download/v#{version}/simplehttpserver_#{version}_macOS_arm64.zip"
-    sha256 "1905f196dcedbfd69fd1ab726eb21893193aaba6e6a1a3cc7ac574383308c55a"
+    sha256 "9d146035db57d6b672857c75960780d5ece85f1e542c0ac6fb8c547ec62823e3"
   else
     url "https://github.com/projectdiscovery/simplehttpserver/releases/download/v#{version}/simplehttpserver_#{version}_macOS_amd64.zip"
-    sha256 "6f94cfd976a73cd8a410f33c119954a661da930226a47c21c83f3c358362f994"
+    sha256 "958b697e11edb2508329253db9688adef6f6c211e92dc89f57d0b7e7b0f787d7"
   end
 
   def install
