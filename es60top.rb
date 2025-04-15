65667c7f5ed80dc21d091c65f85a5f525aacfb87
Vicen Dominguez <vicente.dominguez@flywire.com>
2025-04-15 20:31:43 +0200

    es60top v0.0.4

--- es60top.rb
+++ es60top.rb
@@ -1,14 +1,14 @@
 class Es60top < Formula
   desc "An old ElasticSearch monitoring client like top for the deprecated 6.x versions"
   homepage "https://github.com/vicendominguez/es60top"
-  version "0.0.1"
+  version "0.0.4"
 
   if Hardware::CPU.arm?
     url "https://github.com/vicendominguez/es60top/releases/download/v#{version}/es60top-v#{version}-darwin-arm64"
-    sha256 "8fd526da2a6a040e8bf4f71fa2051d4e899f9732d33eae39e95aa8a1943c1091"
+    sha256 "5b29474249c6bd90d57b51ba3cb503c0df6bb0cf9c4d6c09b292650ae9706817"
   else
     url "https://github.com/vicendominguez/es60top/releases/download/v#{version}/es60top-v#{version}-darwin-amd64"
-    sha256 "053e28c8c30194e116a7cc6837a235eaf5385cfb6dd722aec74dbda2a7370275"
+    sha256 "0a5e4aa802c1dfe54441fc120498798d115645e33cfe542e0b21766252532fbe"
   end
 
   def install
@@ -16,6 +16,6 @@ class Es60top < Formula
   end
 
   test do
-    system "#{bin}/es60top", "-v"
+    system "#{bin}/es60top", "-version"
   end
 end
