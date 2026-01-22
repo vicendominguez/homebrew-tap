db82ffbb639fc3f74f7f83a437337d76ac80bc52
Vicen Dominguez <vicente.dominguez@flywire.com>
2026-01-22 11:53:11 +0100

    Update mediadupes to v0.1.3

--- mediadupes.rb
+++ mediadupes.rb
@@ -1,14 +1,14 @@
 class Mediadupes < Formula
   desc "Fast, parallel media deduplicator with EXIF metadata support"
   homepage "https://github.com/vicendominguez/mediadupes"
-  version "0.1.2"
+  version "0.1.3"
 
   if Hardware::CPU.arm?
     url "https://github.com/vicendominguez/mediadupes/releases/download/v#{version}/mediadupes-darwin-arm64.tar.gz"
-    sha256 "204ca338ef208f09ec9b10cee192f11b8b02415eababb3374f4c7d8b1ed09cd0"
+    sha256 "e2568cc45e0db8221c8c1447b7956ba579aeb1deb5cb29d95154a38990dea4dd"
   else
     url "https://github.com/vicendominguez/mediadupes/releases/download/v#{version}/mediadupes-darwin-amd64.tar.gz"
-    sha256 "dd0e0ccfdef0ea82c4a01636be4b492a4935594454ef3b779b589859aa189a58"
+    sha256 "79845aa4c9d0b956fb60c710dac225708fba33545933856912cfa1ab785e2a73"
   end
 
   depends_on "exiftool"
