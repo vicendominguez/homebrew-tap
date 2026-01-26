47cef3de97a06bda043096dcabde736551799085
Vicen Dominguez <vicente.dominguez@flywire.com>
2026-01-26 10:03:41 +0100

    [master] Update version to 0.0.6 and corresponding SHA256 checksums.

--- gocommit.rb
+++ gocommit.rb
@@ -1,15 +1,15 @@
 class Gocommit < Formula
   desc "Creates commits with automatically generated messages using ollama"
   homepage "https://github.com/vicendominguez/gocommit"
-  version "0.0.5"
+  version "0.0.6"
   license "GPL-3.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/vicendominguez/gocommit/releases/download/v0.0.5/gocommit-v0.0.5-darwin-arm64"
-    sha256 "f5cc3254a18db3efb28cb2ee55ce93590dcdb303164d87fe646a3e65872fcd52"
+    url "https://github.com/vicendominguez/gocommit/releases/download/v0.0.6/gocommit-v0.0.6-darwin-arm64"
+    sha256 "6b807866b469e261298b6c261ec3396cdfdd1fe8adbcb4b565b870d95bc3f945"
   else
-    url "https://github.com/vicendominguez/gocommit/releases/download/v0.0.5/gocommit-v0.0.5-darwin-amd64"
-    sha256 "962696e0efde7e8ba147754adde44afcb8ba49d79e07b08bef859edf76201bbe"
+    url "https://github.com/vicendominguez/gocommit/releases/download/v0.0.6/gocommit-v0.0.6-darwin-amd64"
+    sha256 "a483e4d65927ea0ff49057d7614200776ebf7cf167def0c911f6b2f6bbe0dcb9"
   end
 
   def install
@@ -17,6 +17,6 @@ class Gocommit < Formula
   end
 
   test do
-    assert_match "v0.0.5", shell_output("#{bin}/gocommit -version")
+    assert_match "v0.0.6", shell_output("#{bin}/gocommit -version")
   end
 end
