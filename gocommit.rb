270665ee93736c50a6be4ab5c7353bcd5dd76321
Vicen Dominguez <vicente.dominguez@flywire.com>
2026-01-26 10:00:32 +0100

    [master] Update release with ARM64 and AMD64 binaries for v0.0.6.

--- gocommit.rb
+++ gocommit.rb
@@ -1,14 +1,19 @@
 class Gocommit < Formula
   desc "Creates commits with automatically generated messages using ollama"
   homepage "https://github.com/vicendominguez/gocommit"
-  url "https://github.com/vicendominguez/gocommit/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 "41434cacaf4f0d17b9292c0219a8fef61716f5ca6f263b52e3e9d49c0715e97b"
+  version "0.0.5"
   license "GPL-3.0"
 
-  depends_on "go" => :build
+  if Hardware::CPU.arm?
+    url "https://github.com/vicendominguez/gocommit/releases/download/v0.0.5/gocommit-v0.0.5-darwin-arm64"
+    sha256 "f5cc3254a18db3efb28cb2ee55ce93590dcdb303164d87fe646a3e65872fcd52"
+  else
+    url "https://github.com/vicendominguez/gocommit/releases/download/v0.0.5/gocommit-v0.0.5-darwin-amd64"
+    sha256 "962696e0efde7e8ba147754adde44afcb8ba49d79e07b08bef859edf76201bbe"
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/gocommit"
+    bin.install "gocommit-v#{version}-darwin-#{Hardware::CPU.arch}" => "gocommit"
   end
 
   test do
