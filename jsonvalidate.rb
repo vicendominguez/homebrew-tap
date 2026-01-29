293bcffb07beee29c806d6e3374907f01cc46a7c
Vicen Dominguez <vicente.dominguez@flywire.com>
2026-01-29 13:38:53 +0100

    [master] fix: update jsonvalidate version to 0.1.2

--- jsonvalidate.rb
+++ jsonvalidate.rb
@@ -1,14 +1,14 @@
 class Jsonvalidate < Formula
   desc "A Simple command-line tool for validating JSON files or input. Just checking if it parses"
   homepage "https://github.com/vicendominguez/jsonvalidate"
-  version "0.1.1"
+  version "0.1.2"
 
   if Hardware::CPU.arm?
     url "https://github.com/vicendominguez/jsonvalidate/releases/download/v#{version}/jsonvalidate-v#{version}-darwin-arm64"
-    sha256 "eafbd380479bd6533cc70ba71612d962b69072e24a511f65c32400bccf28ca92"
+    sha256 "1a0519ef48562fb14cc1caa7f6d328c6e353873e9e4130e381bfceb37e7058aa"
   else
     url "https://github.com/vicendominguez/jsonvalidate/releases/download/v#{version}/jsonvalidate-v#{version}-darwin-amd64"
-    sha256 "c29e334db77908ca3ec0e3ecfd08a8f3b1a1f7246cf82f234040821e6d706339"
+    sha256 "c540a314a68dc0cb884e2981c4996d43d9d2b822e2047c977282304c85ec3687"
   end
 
   def install
