0a08c0262e6a85b405616c309ba0b6016accec79
Vicen Dominguez <vicente.dominguez@flywire.com>
2026-02-09 23:10:19 +0100

    [master] chore: update octos version to 0.5.0

--- octos.rb
+++ octos.rb
@@ -1,15 +1,15 @@
 class Octos < Formula
   desc "LLM agent pipeline orchestrator"
   homepage "https://github.com/vicendominguez/octos"
-  version "0.3.0"
+  version "0.5.0"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/vicendominguez/octos/releases/download/v0.3.0/octos-darwin-arm64"
-    sha256 "33f2fdbda3025cec0d6135ee91cd2333c105557364c898fb974d6e2fe428eb08"
+    url "https://github.com/vicendominguez/octos/releases/download/v0.5.0/octos-darwin-arm64"
+    sha256 "abe12d3e199b2d4f17c9ea2c36093486f6101d426b1fd3d2a1a9b328985153d0"
   else
-    url "https://github.com/vicendominguez/octos/releases/download/v0.3.0/octos-darwin-amd64"
-    sha256 "fcf3a1507ef1cab23f53293c1d1e21fc23ac167fb895019807096cf6984cbfdb"
+    url "https://github.com/vicendominguez/octos/releases/download/v0.5.0/octos-darwin-amd64"
+    sha256 "2e9fc89921e5bd6401ab71c29cd66cfa95c0a749f2c5590ec0d6bdba11b4e670"
   end
 
   def install
