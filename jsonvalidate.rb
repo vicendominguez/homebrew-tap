c219b7c087b558744b81040a77300cafc90fc26e
Vicen Dominguez <vicente.dominguez@flywire.com>
2026-06-11 11:34:32 +0200

    jsonvalidate: bump to v0.1.3
    Fix: report all validation errors in recursive mode

--- jsonvalidate.rb
+++ jsonvalidate.rb
@@ -1,14 +1,14 @@
 class Jsonvalidate < Formula
   desc "A Simple command-line tool for validating JSON files or input. Just checking if it parses"
   homepage "https://github.com/vicendominguez/jsonvalidate"
-  version "0.1.2"
+  version "0.1.3"
 
   if Hardware::CPU.arm?
     url "https://github.com/vicendominguez/jsonvalidate/releases/download/v#{version}/jsonvalidate-v#{version}-darwin-arm64"
-    sha256 "1a0519ef48562fb14cc1caa7f6d328c6e353873e9e4130e381bfceb37e7058aa"
+    sha256 "4788257756f076c7150f98fde2d81b59ac6740064ec03da3e0094c4ecaaf112a"
   else
     url "https://github.com/vicendominguez/jsonvalidate/releases/download/v#{version}/jsonvalidate-v#{version}-darwin-amd64"
-    sha256 "c540a314a68dc0cb884e2981c4996d43d9d2b822e2047c977282304c85ec3687"
+    sha256 "aa1ae1e9b64aa81149d5b8df1462088b9589dda80b416b72154364debf8229ab"
   end
 
   def install
