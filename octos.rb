2b4e69c941cab37daa2cd7a02f6f08cf5047a448
Vicen Dominguez <vicente.dominguez@flywire.com>
2026-06-29 23:42:16 +0200

    octos 0.7.2, fix license to GPL-3.0

--- octos.rb
+++ octos.rb
@@ -1,15 +1,15 @@
 class Octos < Formula
   desc "LLM agent pipeline orchestrator"
   homepage "https://github.com/vicendominguez/octos"
-  version "0.7.1"
-  license "MIT"
+  version "0.7.2"
+  license "GPL-3.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/vicendominguez/octos/releases/download/v0.7.1/octos-darwin-arm64"
-    sha256 "c9c9bbedfc2fcef562365c81db37d4dd9c8e546e11be85c29aa02c48c6dd6bb7"
+    url "https://github.com/vicendominguez/octos/releases/download/v0.7.2/octos-darwin-arm64"
+    sha256 "209f7186c7bf8efdebb7fd424de94f74c3d3bbdb994869d125a42471bc9bfa0d"
   else
-    url "https://github.com/vicendominguez/octos/releases/download/v0.7.1/octos-darwin-amd64"
-    sha256 "6c41bb24032ea03291744513658aa813c0220723b31d2d31114d6233d43fade7"
+    url "https://github.com/vicendominguez/octos/releases/download/v0.7.2/octos-darwin-amd64"
+    sha256 "24acbb7b5bf4b870e3bed6b580063964363578edf66a4bef6286bf26a0f62dc9"
   end
 
   def install
