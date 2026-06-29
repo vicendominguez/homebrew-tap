3f999cf2c0a6e86ae24fa758cad8ff8bbd0356cf
Vicen Dominguez <vicente.dominguez@flywire.com>
2026-06-29 22:35:49 +0200

    octos 0.7.1

--- octos.rb
+++ octos.rb
@@ -1,15 +1,15 @@
 class Octos < Formula
   desc "LLM agent pipeline orchestrator"
   homepage "https://github.com/vicendominguez/octos"
-  version "0.7.0"
+  version "0.7.1"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/vicendominguez/octos/releases/download/v0.7.0/octos-darwin-arm64"
-    sha256 "1d35c6b29573e044dcd0b64fe4bbdfd36a877486167819d3b709c8afdedae4e8"
+    url "https://github.com/vicendominguez/octos/releases/download/v0.7.1/octos-darwin-arm64"
+    sha256 "c9c9bbedfc2fcef562365c81db37d4dd9c8e546e11be85c29aa02c48c6dd6bb7"
   else
-    url "https://github.com/vicendominguez/octos/releases/download/v0.7.0/octos-darwin-amd64"
-    sha256 "52d42e7f770dce8ad134b87e2e4061aea9b60067a53ea3c4ee54976e8ed1cc3d"
+    url "https://github.com/vicendominguez/octos/releases/download/v0.7.1/octos-darwin-amd64"
+    sha256 "6c41bb24032ea03291744513658aa813c0220723b31d2d31114d6233d43fade7"
   end
 
   def install
