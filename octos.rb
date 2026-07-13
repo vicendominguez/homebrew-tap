4c5484aa979f467d6f64f0eafb4aab7e757f7f51
Vicen Dominguez <vicente.dominguez@flywire.com>
2026-07-13 17:09:41 +0200

    octos 1.2.2: parallel execution, needs, graph overlay, refactor

--- octos.rb
+++ octos.rb
@@ -1,15 +1,15 @@
 class Octos < Formula
   desc "LLM agent pipeline orchestrator"
   homepage "https://github.com/vicendominguez/octos"
-  version "1.1.5"
+  version "1.2.2"
   license "GPL-3.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/vicendominguez/octos/releases/download/v1.1.5/octos-darwin-arm64"
-    sha256 "0a46642a5bc8a75e40cd2bd54fed86b1ef7150748604623ebb0a866e212dce91"
+    url "https://github.com/vicendominguez/octos/releases/download/v1.2.2/octos-darwin-arm64"
+    sha256 "8215b487163330bb6d11de4044426166abbb37087bca4a3e11e328ab3d66470a"
   else
-    url "https://github.com/vicendominguez/octos/releases/download/v1.1.5/octos-darwin-amd64"
-    sha256 "6128708b0d595fc18b279dcb7c1489d0c33817b3ae15a7a420d3fc6610f933dd"
+    url "https://github.com/vicendominguez/octos/releases/download/v1.2.2/octos-darwin-amd64"
+    sha256 "6345c8eb7e6865711ed3213c76b826a735c5c3e50dcb7d535bcf523d6113b9e4"
   end
 
   def install
